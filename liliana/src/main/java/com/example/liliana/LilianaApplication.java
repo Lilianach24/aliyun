f9e168f45063823200c77bd68f6c1a0ad9bf0ad0
Lilianac <Lilianachenlan@qq.com>
2022-12-09 20:19:40 +0800

    添加主页控制器

---
+++ liliana/src/main/java/com/example/liliana/LilianaApplication.java
@@ -0,0 +1,13 @@
+package com.example.liliana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilianaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LilianaApplication.class, args);
+    }
+
+}
---
+++ liliana/src/test/java/com/example/liliana/LilianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liliana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilianaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
